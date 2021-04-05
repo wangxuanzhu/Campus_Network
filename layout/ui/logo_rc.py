e5bdd6f8d1aba8e24c226a6835c2074fda00ff83
xuanzhu <2389752245@qq.com>
2021-04-05 23:32:54 +0800

    删除了无用语句

--- layout/ui/logo_rc.py
+++ layout/ui/logo_rc.py
@@ -1007,12 +1007,3 @@ def qCleanupResources():
 
 qInitResources()
 
-if __name__ == '__main__':
-    import sys
-    from PyQt5.QtWidgets import QApplication, QMainWindow
-    app = QApplication(sys.argv)
-    MainWindow = QMainWindow()
-    ui = Ui_MainWindow()
-    ui.setupUi(MainWindow)
-    MainWindow.show()
-    sys.exit(app.exec_())
